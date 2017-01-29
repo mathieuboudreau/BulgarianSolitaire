7830d089467a5be85dedba22c3937fbeb600c8c6
mathieuboudreau <emb6150@gmail.com>
2017-01-29 15:37:07 -0330

    Extend generator for cases of summing the two last nums in set

--- bulgariansolitaire/get_search_space.py
+++ bulgariansolitaire/get_search_space.py
@@ -1,5 +1,5 @@
 def get_search_space(totalSet):
-    
+
     def _addend_search(addendSet):
         ''' Generator that returns an addend set (set of numbers which add up to a given number)
 
@@ -13,6 +13,13 @@ def get_search_space(totalSet):
                 addendSet: list of integers.
         '''
         yield addendSet # First call to generator should return the initial set from the generator arg.
+
+        while len(addendSet) > 1:
+            # An addend of length (len(addendSet)-1) can be created by summing the two last values of the a list of length
+            # 2 or more
+            addendSet = addendSet[:-2] + [addendSet[-2] + addendSet[-1]]
+
+            yield addendSet
 
     longestAddend = [1] * totalSet; # Simplest case of set of ones which add up to totalSet.
 
--- test/test_get_search_space.py
+++ test/test_get_search_space.py
@@ -12,6 +12,9 @@ class GetSeachSpaceTest(unittest.TestCase):
     def test_that_get_search_space_returns_list(self):
         assert isinstance(bs.get_search_space(1), list)
 
-    # Return Types
+    # Return known cases
     def test_that_get_search_space_returns_the_longest_set_case(self):
         self.assertTrue([1,1,1,1] in bs.get_search_space(4))
+
+    def test_that_get_search_space_returns_the_single_value_case(self):
+        self.assertTrue([5] in bs.get_search_space(5))
