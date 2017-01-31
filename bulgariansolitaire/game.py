78e35b606952e4117b20f059e20d20ca6667e9ef
mathieuboudreau <emb6150@gmail.com>
2017-01-31 19:12:57 -0330

    Added class doc info for new methods

--- bulgariansolitaire/game.py
+++ bulgariansolitaire/game.py
@@ -3,11 +3,23 @@ class Game:
     '''Bulgarian Solitaire game class.
 
     --initializer args--
-        1st arg: Positive integer that represents the number of cards used in the game.
+        (optional) 1st arg: Positive integer that represents the number of cards used in the game. 
 
     --class variables--
         numCards: Positive integer that represents the number of cards used in the game.
         gameHistory: List containing all the pileSets (lists) of the most recent games for each step until convergence.
+
+    --methods--
+        run(pileSet): Executes a Bultarian Solitaire game with the starting piles of cards described by the argument (list of 
+                      integers). Zero piles are ignored. The history of the card piles following each game loop are saved in 
+                      the class variable "gameHistory" as a list of lists, and is also returned by the method call. Each
+                      call of run will reset the previous gameHistory variable prior to saving the current game.
+
+                      Example: run([1 9 10]) will run a Bulgarian Solitaire games using 20 cards, using three piles (1, 9,
+                      and 10 card tall).
+    
+        print_history(): Print the previous game history in a human readable format.
+
     '''
     def __init__(self, *args):
         if args:
@@ -26,7 +38,7 @@ class Game:
         return newSet
 
     def run(self, pileSet):
-        ''' Run a Bulgarian Solitair game.
+        ''' Run a Bulgarian Solitaire game.
         
         Each game step until the solution is saved in the instance variable "gameHist".
         Returns self.gameHist
